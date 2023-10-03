03745c731f67071f6c2813685fc4aca19e203579
jeremyLee0512 <2410250284@qq.com>
2023-10-03 19:38:54 +0800

    HelloGodot init

---
+++ HelloGodot/main.gd
@@ -0,0 +1,23 @@
+extends Node3D
+class_name Main
+
+var s_cube: PackedScene = preload("res://cube.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+#	print('Hello Godot!')
+	add_child(s_cube.instantiate())
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+#	add_child(s_cube.instantiate())
+	pass
+
+var testArray : PackedStringArray = ['asda', 123]
+
+##This function greets the provided name.
+##	Args:
+##	name (String): The name to greet.
+func greet(name):
+	print("Hello, " + name + "!")
